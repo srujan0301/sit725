220a61967e875bb95c18c606b53eb96ae6c9293e
Srujan <srujank0301@gmail.com>
2025-04-14 21:13:41 +1000

    Delete 5.2 directory

--- 5.2/README.md
+++
@@ -1,3 +0,0 @@
-SIT725 Task 5.2cP - 
-
-This project demonstrates an Express.js application using the MVC architecture with MongoDB and Materialize CSS. It fetches and displays project cards dynamically from a MongoDB collection.
--- 5.2/routes/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const projectController = require("../controllers/projectController");
-
-router.get("/", projectController.getAllProjects);
-
-module.exports = router;
